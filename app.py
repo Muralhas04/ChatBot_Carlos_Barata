d379c4ec9dde3e3118b46292f64fb0dcaa68e376
Muralhas04 <luisfaiscacandeias@gmail.com>
2026-06-01 12:47:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def obter_resposta(texto: str) -> str:
    respostas = {
         ('olá', 'boa tarde', 'bom dia'): 'Olá tudo bem!',
         'como estás': 'Estou bem, obrigado!',
-    #     ('bye', 'adeus', 'tchau'): 'Gostei de falar contigo! Até breve...',
+   ('bye', 'adeus', 'tchau'): 'Gostei de falar contigo! Até breve...',
     # }
 
     # for chave, resposta in respostas.items():
